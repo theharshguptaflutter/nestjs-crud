f36c5896a0a75f54783d020d7f6b5826f9224ed4
harsh gupta <theharshgupta2020@gmail.com>
2024-11-23 17:46:03 +0530

    users routes created

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Res } from '@nestjs/common';
 import {UserService} from './user.service';
 @Controller('user')
 export class UserController {
     constructor(private UserService: UserService) {}
+
+
+    @Post('register')
+    async register(@Body() body, @Res() res) {
+      await this.UserService.register(body, res);
+    }
+
+    @Get('users')
+    async getAllUsers(@Res() res) {
+      return await this.UserService.getAllUsers(res);
+    }
+    @Get('user/:user_id')
+    async getAllUserById(@Param() param, @Res() res) {
+      return await this.UserService.getUserById(param,res);
+    }
 }
